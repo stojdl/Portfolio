649ce1c687ceb3cb3c5a311baf91cbbe2efb33a6
stojdl <bedrich.stojdl@email.cz>
2024-10-08 13:15:37 +0200

    úprava interakce FE

--- resources/js/Components/Tiles/ShowOfferTile.tsx
+++ resources/js/Components/Tiles/ShowOfferTile.tsx
@@ -5,7 +5,7 @@ const ShowOfferTile = (props: ShowOfferTileProps) => {
     return (
         <div
             {...props}
-            className={`w-full px-2 py-4 flex flex-col space-y-4 bg-slate-300 dark:bg-slate-700 border border-slate-400 rounded drop-shadow-xl ${className}`}
+            className={`w-full px-2 py-4 flex flex-col space-y-4 bg-slate-300 hover:bg-slate-200 dark:bg-slate-700 dark:hover:bg-slate-800 border border-slate-400 rounded drop-shadow-xl ${className}`}
         >
             <div className="flex flex-col space-y-2 font-bold md:flex-row md:space-y-0 md:space-x-8 ">
                 <div>{name}</div>
