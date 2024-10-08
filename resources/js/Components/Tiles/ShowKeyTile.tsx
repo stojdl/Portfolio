072603b96cd544a2ac59e6d233f31873d17bb67d
stojdl <bedrich.stojdl@email.cz>
2024-10-08 13:19:43 +0200

    přidán FE pro zneplatňování klíčů

--- resources/js/Components/Tiles/ShowKeyTile.tsx
+++ resources/js/Components/Tiles/ShowKeyTile.tsx
@@ -1,11 +1,12 @@
 import { ShowKeyTileProps } from "@/types/ShowKeyTile";
+import { MdOutlineTimerOff } from "react-icons/md";
 
 const ShowOfferTile = (props: ShowKeyTileProps) => {
     const { className, keyFor, value, valid, used } = props;
     return (
         <div
             {...props}
-            className={`w-full px-2 py-4 flex justify-between bg-slate-300 hover:bg-slate-200 dark:bg-slate-700 dark:hover:bg-slate-800 border border-slate-400 rounded drop-shadow-xl ${className}`}
+            className={`w-full px-2 py-4 flex items-center justify-between bg-slate-300 hover:bg-slate-200 dark:bg-slate-700 dark:hover:bg-slate-800 border border-slate-400 rounded drop-shadow-xl ${className}`}
         >
             <div className="flex flex-col space-y-2 font-bold">
                 <div>Pro: {keyFor}</div>
@@ -15,6 +16,7 @@ const ShowOfferTile = (props: ShowKeyTileProps) => {
                 <div>Platný: {valid ? "Ano" : "Ne"}</div>
                 <div>Byl použit: {used ? "Ano" : "Ne"}</div>
             </div>
+            <MdOutlineTimerOff className="w-6 h-6" />
         </div>
     );
 };
