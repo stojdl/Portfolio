8cc66ca1f94930bb5041aca4ebbaec5d58f1bbce
stojdl <bedrich.stojdl@email.cz>
2024-10-08 17:30:49 +0200

    button color fix

--- resources/js/Components/LinkButton.tsx
+++ resources/js/Components/LinkButton.tsx
@@ -6,7 +6,7 @@ const LinkButton = (props: LinkButtonProps) => {
     const { children, icon, arrow = false } = props;
     return (
         <Link
-            className="w-full px-4 py-2 flex items-center justify-center border rounded space-x-2 bg-slate-200 dark:bg-slate-700 border-slate-300 md:px-6 md:w-max hover:bg-slate-300 dark:hover:bg-slate-600 dark:hover:text-neutral-200"
+            className="w-full px-4 py-2 flex items-center justify-center border rounded space-x-2 bg-slate-200 dark:bg-slate-700 border-slate-300 text-black dark:text-slate-100 md:px-6 md:w-max hover:bg-slate-300 dark:hover:bg-slate-600 dark:hover:text-neutral-200"
             {...props}
         >
             {icon && icon}
